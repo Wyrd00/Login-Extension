3bf48fc9ab70971faee479c4d4de87f8d44f4c4d
IOLevi <337@holbertonschool.com>
2019-11-02 18:59:04 -0700

    pushing for sync

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+    chrome.storage.sync.set({username: 'admin'}, function() {
+      console.log('The username is hello');
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
--- options.js
+++ options.js
@@ -1,15 +1,43 @@
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
+function handleSubmit() {
+  let credentialsForm = $('#credentials-form');
+
+  credentialsForm.on('submit', function (e) {
+    e.preventDefault();
+    let userName = credentialsForm.find('input[name]="username"');
+    let password = credentialsForm.find('input[name]="password');
+  });
+
+
+  chrome.storage.local.set({
+    username: 'shitbag'
+  }, function (e) {
+    console.log('value is ' + shitbag);
+  })
+
+};
+
+function restoreOptions() {
+  chrome.storage.sync.get({
+    // username and password
+  }, function (c) {
+    
   }
 }
-constructOptions(kButtonColors);
+
+// let page = document.getElementById('buttonDiv');
+// const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+// function constructOptions(kButtonColors) {
+//   for (let item of kButtonColors) {
+//     let button = document.createElement('button');
+//     button.style.backgroundColor = item;
+//     button.addEventListener('click', function() {
+//       chrome.storage.sync.set({color: item}, function() {
+//         console.log('color is ' + item);
+//       })
+//     });
+//     page.appendChild(button);
+//   }
+// }
+// constructOptions(kButtonColors);
+
+$(document).on('DOMContentLoaded', restoreOptions);
