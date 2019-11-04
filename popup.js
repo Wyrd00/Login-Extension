84e82f804dd8074a28538a6dbd99226dface761b
Wyrd00 <amy.tai0120@gmail.com>
2019-11-04 14:54:03 -0800

    dynamic boom

--- popup.js
+++ popup.js
@@ -1,14 +1,31 @@
 // popup.js
 // do I need to inject code into browser chrome.tabs.executeScript?
 
-function setEventListeners() {
+function populateCredentials() {
+  chrome.storage.sync.get(['credentials'], function (credentials) {
+    console.log(credentials)
+    let cred_list = $('#credential-list')
+    for (credential of credentials.credentials) {
+      let li = document.createElement("li");
+      li.append(document.createTextNode(credential.username));
+      let button = document.createElement("button")
+      button.setAttribute('class', 'login-btn')
+      button.innerHTML = "login";
+      li.append(button);
+      cred_list.append(li)
+    }
+  })
+}
+
+function setEventListeners(e) {
   console.log('setting event listeners in popup');
-  const loginBtn = document.getElementById('login-btn');
+  const loginBtn = e.target
   loginBtn.onclick = function () {
     chrome.tabs.executeScript({
       file: 'login.js'
-    });    
+    });
   }
 }
 
-setEventListeners();
+$(document).on('DOMContentLoaded', populateCredentials);
+$(document).on('click', '.login-btn', setEventListeners)
