907a2625c994c3b18d7f4e2a198b41396980e81b
IOLevi <337@holbertonschool.com>
2019-11-04 11:46:47 -0800

    ctrl+shift+y added as command to background script; will inject the login and run

--- background.js
+++ background.js
@@ -1,22 +1,32 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({greeting: 'yo'}, function() {
-      console.log('The username is hello');
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {urlContains: 'education.com'},
-          })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({
+    greeting: 'yo'
+  }, function () {
+    console.log('The username is hello');
   });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            urlContains: 'education.com'
+          },
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
 
-  chrome.commands.onCommand.addListener(function(command) {
-    console.log('Command:', command);
-  });
+chrome.commands.onCommand.addListener(function (command) {
+  console.log('Command:', command);
+  if (command === 'toggle-login') {
+    console.log('logging in from keypress');
+    chrome.tabs.executeScript({
+      file: 'login.js'
+    });
+  }
+});
 
 //   chrome.runtime.onMessage.addListener((msg, sender, response) => {
 //     switch (msg.type) {
