2150a7337fcd9ba75e5d308beabbfdf745f184d9
IOLevi <337@holbertonschool.com>
2019-11-05 11:06:44 -0800

    refactor options js for readability

--- src/login.js
+++ src/login.js
@@ -1,8 +1,8 @@
 //  Login.js 
 
-function main() {
+function attemptLoginFromCredential(cred) {
     try {
-        const cred = getFirstCredential();
+        // const cred = getFirstCredential();
         const loginFormData = populateFormDataLogin(cred);
         login(loginFormData);
     }
@@ -11,16 +11,16 @@ function main() {
     }
 }
 
-function getFirstCredential() {
-    console.log('retrieving first credential');
-    let firstCredential;
-    chrome.storage.sync.get(['credentials'], function (c) {
-        console.log(c);
-        firstCredential = c.credentials[0];
-    });
-    console.log('first credential is ', firstCredential);
-    return firstCredential;
-}
+// function getFirstCredential() {
+//     console.log('retrieving first credential');
+//     let firstCredential;
+//     chrome.storage.sync.get(['credentials'], function (c) {
+//         console.log(c);
+//         firstCredential = c.credentials[0];
+//     });
+//     console.log('first credential is ', firstCredential);
+//     return firstCredential;
+// }
 
 async function login(loginFormData) {
     const response = await makeRequest('/api/login', 'POST', loginFormData);
@@ -60,4 +60,16 @@ function populateFormDataLogin(c) {
     return bodyFormData;
 }
 
-main();
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        switch(message.type) {
+            case "loginAttempt":
+                attemptLoginFromCredential(message.data);
+                sendResponse('heres a response from login');
+                break;
+            default:
+                sendResponse('unknown action type');
+                break;
+        }
+    }
+);
--- src/popup.js
+++ src/popup.js
@@ -1,30 +1,43 @@
 // popup.js
 
+function loginMessageWithCredential(cred) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type:"loginAttempt", data: cred}, function(response){
+      console.log('Received response from login attempt -- can do updates to popup with response: ', response);
+    });
+  });
+}
+
 function populateCredentials() {
   chrome.storage.sync.get(['credentials'], function (syncedCredentials) {
     console.log(syncedCredentials);
-    let cred_list = $('#credential-list')
+    let cred_list = $('#credential-list');
     for (let credential of syncedCredentials.credentials) {
       let li = document.createElement("li");
       li.append(document.createTextNode(credential.username));
       let button = document.createElement("button");
+      // AMY: Added this here so that each button calls login with its own credential
+      button.onclick = function () {
+        loginMessageWithCredential(credential);
+      };
       button.setAttribute('class', 'login-btn');
       button.innerHTML = "login";
       li.append(button);
-      cred_list.append(li)
+      cred_list.append(li);
     }
   })
 }
 
-function setEventListeners(e) {
-  console.log('setting event listeners in popup');
-  const loginBtn = e.target
-  loginBtn.onclick = function () {
-    chrome.tabs.executeScript({
-      file: 'login.js'
-    });
-  }
-}
+// function setEventListeners(e) {
+//   console.log('setting event listeners in popup');
+//   const loginBtn = e.target;
+//   loginBtn.onclick = function () {
+//     chrome.tabs.executeScript({
+//       file: 'login.js'
+//     });
+//   }
+// }
 
 $(document).on('DOMContentLoaded', populateCredentials);
-$(document).on('click', '.login-btn', setEventListeners);
+
+
